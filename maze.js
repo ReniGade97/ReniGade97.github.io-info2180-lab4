a5a3a017f2bb040b8011dc9bf81e8e0a74859dee
Renee Samms <rsamms97@gmail.com>
2018-10-23 23:47:51 -0500

    error msgs

--- maze.js
+++ maze.js
@@ -2,7 +2,6 @@
 // Web Development
 // 620088389
 
-// Global variables
 var boundaries;
 var end;
 var start;
@@ -11,11 +10,54 @@ var mazeBoundary;
 var win = false;
 var outOfBounds = false;
 
-// Sets up any necessary event handlers on page
 window.onload = function() {
-  boundaries  = document.querySelectorAll(".boundary" []);
-  end         = document.getElementById("end");
-  start       = document.getElementById("start");
-  status      = document.getElementById("status");
-  mazeBoundary= document.getElementById("maze");
+    boundaries  = document.querySelectorAll(".boundary");
+    end         = document.getElementById("end");
+    start       = document.getElementById("start");
+    status      = document.getElementById("status");
+    mazeBoundary= document.getElementById("maze");
+
+    start.onclick = resetGame;
+    mazeBoundary.onmouseleave = noCheating;
+    end.onmouseover = checkWin;
+    for (var i = 0; i < boundaries.length - 1; i++) {
+		boundaries[i].onmouseover = overBoundary;
+  }
+}
+
+function overBoundary(){
+	if (!win){
+		if(!outOfBounds)
+		{
+			outOfBounds = true;
+			status.innerText = 'You Lose! Select "S" to try again.';
+			for (var i = 0; i < boundaries.length - 1; i++) {
+			boundaries[i].className += " youlose";
+			}
+		}
+	}
+}
+
+function checkWin(){
+	if(!outOfBounds){
+		win = true;
+		statusState.innerText = 'You Win! Select "S" to try again.';
+		}
+
+}
+
+function resetGame(){
+	win = false;
+	outOfBounds = false;
+	status.innerText = 'Move your mouse over the "S" to begin.';
+	for (var i = 0; i < boundaries.length - 1; i++) {
+		boundaries[i].className = "boundary";
+	}
+}
+
+function noCheating(){
+	if(!win){
+		overBoundary();
+		}
+
 }
